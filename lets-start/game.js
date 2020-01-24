a2e97febcc99c3ce0b9eef5c154994e223423d85
Ricnoceronte <ricnoceronte@gmail.com>
2020-01-24 12:37:13 -0600

    Add files via upload

--- lets-start/game.js
+++ lets-start/game.js
@@ -11,10 +11,11 @@ let balas2 =[]
 
 
 const img  = {
-    personaje1: "./assets/scar.png",
     bg: "./assets/bgcy.jpg",
     char:"./assets/char.png",
-    nu:"./assets/nu.png"
+    nu:"./assets/nu.png",
+    boss:"./assets/btn.png"
+
 }
  let leo = img.char
  let nu = img.nu
@@ -29,13 +30,32 @@ const img  = {
     this.image.src = img.bg
     this.image.onload= ()=>{
         this.draw()
-
     }
  }
     draw(){
         ctx.drawImage(this.image, 0,0,canvas.width,canvas.height)
     }
    
+ }
+
+
+
+
+ class Boss{
+     constructor(){
+         this.image = new Image()
+         this.image.src= img.boss
+         this.image.onload=()=>{
+             this.draw()
+         }
+
+    }
+     draw(){
+
+            ctx.drawImage(this.image, (canvas.width/3)+50,0,300,300)
+        
+
+        }
  }
 
 class Personaje{
@@ -136,10 +156,10 @@ class Personaje2{
         this.x = x
         this.y = y
         this.width = 85
-        this.height = 240
+        this.height = 480
         this.sx =0
         this.sy=0
-        this.alturaprot= 240
+        this.alturaprot= 250
         this.fuerzadesalto=12
         this.vy= .03
         this.vx=0
@@ -158,13 +178,13 @@ class Personaje2{
         }else{
             this.vy ++
         }
-         if(this.sx>=595){
-            this.sx = 0}
+         if(this.sx<=0){
+            this.sx = 269}
 
         ctx.drawImage(this.image,
             this.sx,
             this.sy,
-            85*-1,
+            (269/7)*(-1) ,
             240,
             this.x,
             this.y,
@@ -232,10 +252,10 @@ class bullet{
     this.y+= this.vy
     this.x+=this.vx
     if (this.y<0){
-        this.y = this.y
+    this.y = this.y
     }
 
-    ctx.beginPath()
+   ctx.beginPath()
    ctx.arc(this.x, this.y, 35,0,2*Math.PI,false)
    ctx.fill();
 
@@ -254,45 +274,37 @@ hintboss(){
 const friction = 0.8
 const gravity = 0.98
 const background = new Bg()
+// const boss= new Boss()
 const protaGonista = new Personaje(0,canvas.height-240,img)
 const prot2  = new Personaje2(canvas.width-150,canvas.height, img)
-console.log(prot2);
 const bolita =  new bullet(protaGonista.x,protaGonista.y)
+const boss = new Boss()
 
 function update(){
     ctx.clearRect(0,0, canvas.width, canvas.height)
         background.draw()
+        boss.draw()
         protaGonista.draw()
         prot2.draw()
+        generatebullets()
 
     protaGonista.y +=protaGonista.vy
     protaGonista.x += protaGonista.vx
     protaGonista.y += gravity
-        // if (varShoot) {
-      
-        //     // if (bolita.y<0){
-        //     //     bolita.y = canvas.height-240
-        //     //     bolita.x = protagonista.x
-        //     //     }else{
-        //     //     bolita.y--
-
-        //     }
-
-            // bolita.draw()
-
-        
-        
 
 
 }
 
   
+function lifeBars(){
+    ctx.beginPath();
+    ctx.rect(20, 20, 150, 100);
+    ctx.stroke();
+
+}
 
 
-
-
-
-    setInterval(update,1000/60)
+   setInterval(update,1000/60)
 
 
 
@@ -302,7 +314,6 @@ function update(){
         
         const positioniBulletx = protaGonista.x;
         const positioniBullety =  protaGonista.y+500
-        console.log(protaGonista.y);
    
     balas1.push(new bullet(120,canvas.height-240 , false))
 
@@ -334,11 +345,11 @@ keys[keyCode]=true
     }else if (keyCode==82) {
         protaGonista.shoootboos()
 // varShoot=true        
-    }else if(keyCode==102){
+    }else if(keyCode==80){
         prot2.rigth()
-    }else if(keyCode==100){
+    }else if(keyCode==79){
         prot2 .left()
-    }else if (keyCode==76) {
+    }else if (keyCode==16) {
         prot2 .shoootboos()
 // varShoot=true        
     }
@@ -353,3 +364,4 @@ document.addEventListener('keyup', ({keyCode})=>{
 })
 
 
+lifeBars()
