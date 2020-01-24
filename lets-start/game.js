21d98c3e0789e3c23c021a6f7121014fda611716
Ricardo Leyva Gonz�lez <hola@legoricardo.com>
2020-01-24 06:19:07 -0600

    He logrado arreglar varias cosas pero aún esta en proceso

--- lets-start/game.js
+++ lets-start/game.js
@@ -4,7 +4,10 @@ const keys = []
 let frames =0;
 let interval
 let current =0
- let variabledePrueba =false
+let balas1 = []
+let balas2 =[]
+ let varShoot =false
+ let counterhints =0
 const img  = {
     personaje1: "./assets/scar.png",
     bg: "./assets/bgcy.jpg",
@@ -20,7 +23,7 @@ const img  = {
 
     this.image = new Image()
     this.image.src = img.bg
-    this.image.onloadw= ()=>{
+    this.image.onload= ()=>{
         this.draw()
 
     }
@@ -41,7 +44,10 @@ class Personaje{
         this.height = 240
         this.sx =0
         this.sy=0
-        this.vely= .03
+        this.alturaprot= 240
+        this.fuerzadesalto=12
+        this.vy= .03
+        this.vx=0
         this.gravity =.98
         this.image = new Image()
         this.image.src = img.char
@@ -51,34 +57,20 @@ class Personaje{
              
     }
      draw(){
-         // if(this.sx>=595){
-         //    this.sx = 0}
-        // ctx.drawImage(
-        //     this.image,
-        //     this.sx,
-        //     this.sy,
-        //     85,
-        //     240,
-        //     this.x,
-        //     this.y,
-        //     this.width,
-        //     this.height 
-        //     )
 
-        // this.y+=this.vely
-        // if(this.y>canvas.height-240 ){
-        //         this.y= canvas.height-240
+        if(this.y >canvas.height-this.alturaprot){
+            this.y = canvas.height -this.alturaprot
+        }else{
+            this.vy ++
+        }
+         if(this.sx>=595){
+            this.sx = 0}
 
-        //     this.vely=0
-        // }
-        // ctx.beginPath()
-        // ctx.arc(200,200,80,0, 2*Math.PI,false)
-        // ctx.stroke()
-            ctx.drawImage(this.image,
-                this.sx,
-                this.sy,
-                85,
-                240,
+        ctx.drawImage(this.image,
+            this.sx,
+            this.sy,
+            85,
+            240,
             this.x,
             this.y,
             this.width,
@@ -87,147 +79,131 @@ class Personaje{
 
     }
 
+
+
        salta(){
-        this.y-=400
-        this.draw()
-        this.vely=0.03
 
-
-    }
-
-   shoot(){
-
-        this.dy-=1
-    if (this.x> canvas.width/2){
-        this.dx= (-1)*dx
-        
-    }    
-        ctx.beginPath();
-        ctx.arc(this.x+85 +this.dx, this.y+50+this.dy, Math.random()*80, 0, 2 * Math.PI);
-        ctx.fill()
-        ctx.stroke();
-        console.log("hola "+this.dx);
-        // ctx.beginPath();
-        // ctx.arc(this.x, this.y, this.radio, 0, 2 * Math.PI);
-        // ctx.fillStyle= "yellow";
-        // ctx.fill();
-
-        ctx.drawImage(this.image, this.x,this.y,150,400)
+        this.vy = -this.fuerzadesalto*2 
     }
 
     rigth(){
-        this.x +=10;
+        this.x +=30;
         this.move()
-
     }
     left(){
-        this.x  -=10;
+        this.x-=30;
         this.move()
     }
-
     move(){
         this.sx+=85
-        this.x +=30;
 
     }
-    left(){
-        this.x -30;
-    }
-
-
-    saltaAdelante(){
-        // this.y-=100
-        // this.x+=200
-        // this.draw()
-        // this.vely=0.03
-
-    }
-    saltaAtras(){
-let distancia= this.x-200
-
-        // while(this.x>distancia){
-        //     this.y-=2
-        //     this.x-=10
-        //     this.draw()
-        //     this.vely=0.03
-        // }
-
-
-    }
-
-
 }
+
+
+
+const friction = 0.8
+const gravity = 0.98
 const background = new Bg()
 const protaGonista = new Personaje(0,canvas.height-240,img)
 
+
+
+
+
+class bullet{
+  constructor(x,y) {
+    this.x = x
+    this.y = y
+    this.vy=2
+    this.vx =3
+    
+
+}
+  draw() {
+    this.y+= this.vy
+    this.x+=this.vx
+
+    ctx.beginPath()
+   ctx.arc(this.x, this.y, 35,0,2*Math.PI,false)
+   ctx.fill();
+
+}
+}
+
 function update(){
     ctx.clearRect(0,0, canvas.width, canvas.height)
-    setInterval(update,1000/30)
+        background.draw()
+
     protaGonista.draw()
-}
-function start(){
-    update()
+    protaGonista.y +=protaGonista.vy
+    protaGonista.x += protaGonista.vx
+    protaGonista.y += gravity
+    if(varShoot){
+          generatebullets()
+    drawbullets()
+    }
+    bullet.y+=protaGonista.y
+    
+
 }
 
-start()
-// const bolita =new Personaje(0,canvas.height-240,img)
-   
-//     function update(){
-//         setInterval(update,1000/5)
-//         ctx.clearRect(0,0,canvas.width,canvas.height)
-       
-//         bolita.draw()
-//          if(variabledePrueba){
-//             bolita.shoot()
-//          }
+  
+
+
+
+
+
+    setInterval(update,1000/60)
+
+
+
+
+
+    function generatebullets() {
         
-
-//     // const background = new Bg()
-//     const bolita =new Personaje(0,canvas.height-400,80,img)
+        const positioniBulletx = protaGonista.x;
+        const positioniBullety =  protaGonista.y+500
+        console.log(protaGonista.y);
    
-//     function update(){
-//         // intervalo = requestAnimationFrame(update)
-//         setInterval(update,1000/120)
-//         ctx.clearRect(0,0,canvas.width,canvas.height)
-//         // background.draw()
+    balas1.push(new bullet(120,canvas.height-240 , false))
 
-//         bolita.draw()
-       
-//     }
-
-//         function start(){
-//             update()
-//         }
-//         start() 
+    // balas2.push(new bullet()
     
+  
+}
+
+
+function drawbullets() {
+
+  balas1.forEach(bullet => bullet.draw())
+}
 
 
 
-// document.addEventListener('keydown', ({keyCode})=>{
-// keys[keyCode]=true
 
 
-//     if(keys[32]&&keys[39]){
-//         bolita.saltaAdelante()
-//     }else if(keys[32]&&keys[37]){
-//         bolita.saltaAtras()
-//     }else if (keyCode==82) {
-//        variabledePrueba = true
-//     }else if(keyCode==37){
-//         bolita.left()
-//     }else if(keyCode==39){
-//         bolita.rigth()
-//     }else if(keyCode==32){
-//         bolita.salta()
-//     }
-// })
+document.addEventListener('keydown', ({keyCode})=>{
+keys[keyCode]=true
 
 
-// document.addEventListener('keyup', ({keyCode})=>{
-//   variabledePrueba=false
-//   keys[keyCode]=false
+  if(keyCode==37){
+        protaGonista.left()
+    }else if(keyCode==39){
+        protaGonista.rigth()
+    }else if(keyCode==32){
+        protaGonista.salta()
+    }else if (keyCode==82) {
+varShoot=true        
+    }
+})
+
+
+document.addEventListener('keyup', ({keyCode})=>{
+  varShoot=false
+  keys[keyCode]=false
    
     
-// })
+})
 
 
