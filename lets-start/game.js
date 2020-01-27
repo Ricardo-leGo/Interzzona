c3b386c423239ade9cf2d0c887904acebdd563f5
Ricnoceronte <ricnoceronte@gmail.com>
2020-01-27 05:40:17 -0600

    Todo

--- lets-start/game.js
+++ lets-start/game.js
@@ -1,62 +1,151 @@
 let canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
+
+
+
+/*---------------------------------------------------*/
+
+var container_Three = document.querySelector(".uno");
+
+let x = container_Three.clientWidth; 
+let  y = container_Three.clientHeight;
+
+
+
+let  aspect = (x/y) /*Consistent whit css*/;
+let  section = x;
+
+let scene = new THREE.Scene();
+scene.fog = new THREE.Fog( 0x000000, 0.1, 3 );
+var light = new THREE.PointLight( 0xffffff )
+let camara = new THREE.PerspectiveCamera(
+        /*fov*/         36,
+        /*aspect*/      1.5,
+        /*near*/        5,
+        /*far*/         1000 );
+// scene.background = new THREE.Color(0xe1ddd2)
+let renderer = new THREE.WebGLRenderer({alpha:true});
+
+/*******************************/
+
+
+
+
 const keys = []
 let frames =0;
 let interval
 let current =0
 let balas1 = []
 let balas2 =[]
- let varShoot =false
- let counterhints =0
+let varShoot =false
+let varShoot2 =false
+let counterhints =0
 
+        let i = 0
+
+let life1 = 50
+let life2 = 50
+let lifeboss = 5000
+let segundos = 60
 
 const img  = {
     bg: "./assets/bgcy.jpg",
     char:"./assets/char.png",
-    nu:"./assets/nu.png",
-    boss:"./assets/btn.png"
-
+    nu:"./assets/nu2.png",
+    boss: "./aseets/boss.png"
 }
- let leo = img.char
- let nu = img.nu
 
-
-
- class Bg{
- constructor(){
+class Bg{
+    constructor(){
 
 
     this.image = new Image()
     this.image.src = img.bg
     this.image.onload= ()=>{
         this.draw()
+
     }
  }
     draw(){
         ctx.drawImage(this.image, 0,0,canvas.width,canvas.height)
     }
-   
  }
 
-
-
-
- class Boss{
-     constructor(){
-         this.image = new Image()
-         this.image.src= img.boss
-         this.image.onload=()=>{
-             this.draw()
-         }
-
+class FinalBoss{
+    constructor(a){
+    this.a = a
+    this.x = canvas.width/2
+    this.y = 100
+    this.b = 0
+    // this.parametro = b * Math.PI / 180
+    this.radio = 100
+    this.vx = -3
+    this.vy = -3
+    this.colorsBoss = ['rgb(255,90,130)','rgb(189,45,255)','rgb(90,139,121)' ]
     }
-     draw(){
 
-            ctx.drawImage(this.image, (canvas.width/3)+50,0,300,300)
-        
+    draw(){
 
+
+        if (i<360) {
+            i++
+        }else{
+            i=0
         }
- }
+
+
+        let ang = (i*Math.PI)/180
+        let sen = (Math.sin(ang))
+        this.y = sen
+
+            ctx.beginPath()
+            ctx.arc(
+                this.x,
+                this.y*100*(this.a*.2)+60*this.y,
+                this.radio,
+                0,
+                Math.PI,
+                false)
+            ctx.fillStyle = this.colorsBoss[this.a]
+            ctx.fill()
+    }
+    atack(){
+        let angAtack = Math.PI/11
+    
+        if(frames>= 400){
+            
+            
+         for (let i = 0; i < 12; i++) {
+                ctx.beginPath()
+                ctx.arc(
+                canvas.width/2  + Math.cos(angAtack*i)*this.radio,
+                50            +  Math.sin(angAtack*i)*this.radio,
+               15,
+                0,
+                2*Math.PI,
+                false)
+                ctx.shadowColor = 'white';
+                ctx.shadowBlur = 5;
+                ctx.fillStyle = 'rgb(255,90,130)'
+                ctx.fill()
+                ctx.closePath()
+            }
+
+
+            this.radio+=1
+            if(this.radio >canvas.width){
+                this.radio = 100
+            }
+
+             if (this.radio == Math.floor(protaGonista.y)){
+
+            life1 = life1 -10  
+            life2 = life2 -10
+            } 
+             }
+    }
+}
+
 
 class Personaje{
     constructor(x,y,img){
@@ -80,7 +169,7 @@ class Personaje{
         }
              
     }
-     draw(){
+    draw(){
 
         if(this.y >canvas.height-this.alturaprot){
             this.y = canvas.height -this.alturaprot
@@ -89,8 +178,7 @@ class Personaje{
         }
          if(this.sx>=595){
             this.sx = 0}
-
-        ctx.drawImage(this.image,
+    ctx.drawImage(this.image,
             this.sx,
             this.sy,
             85,
@@ -100,16 +188,14 @@ class Personaje{
             this.width,
             this.height 
                 )
-
     }
 
 
 
-       salta(){
+    salta(){
 
         this.vy = -this.fuerzadesalto*2 
     }
-
     rigth(){
         this.x +=30;
         this.move()
@@ -118,37 +204,40 @@ class Personaje{
         this.x-=30;
         this.move()
     }
-    move(){
+     move(){
         this.sx+=85
-
     }
 
 
-    shoting(){
-
-            posx = this.x
-            posy -= this.y
-
-
-
+   generatebullets() {
+        const positioniBulletx = protaGonista.x;
+        const positioniBullety =  protaGonista.y+500
         
-        let posx=0, poy=0, velx=0,vely=0, radio=45
-        ctx.beginPath()
-        ctx.ctx.arc(posx, posy,radio , 0, Math.PI*2, false);
-        ctx.fill()
 
+        if (balas1.length >=20 && balas1[10].y<0 ){
+            balas1.splice(0,19)
+        }else{
 
-
+            balas1.push(new bullet(
+               (protaGonista.x+ 85)+(Math.random()*50),
+                (protaGonista.y+50)+Math.random()*40,
+                Math.random()*35,false))
+        }
 
     }
 
+    drawbullets() {
+                balas1.forEach(bullet => bullet.draw())
+                if (varShoot){
+                    lifeboss=lifeboss -20
+                    console.log(lifeboss);
+                    if(lifeboss<=0){
+                        win()
+                        console.log(lifeboss);
+                    }
+                }
+    }
 }
-
-
-
-
-
-
 class Personaje2{
     constructor(x,y,img){
         this.dx=0
@@ -156,10 +245,10 @@ class Personaje2{
         this.x = x
         this.y = y
         this.width = 85
-        this.height = 480
+        this.height = 240       
         this.sx =0
         this.sy=0
-        this.alturaprot= 250
+        this.alturaprot= 240
         this.fuerzadesalto=12
         this.vy= .03
         this.vx=0
@@ -171,20 +260,20 @@ class Personaje2{
         }
              
     }
-     draw(){
+    draw(){
 
         if(this.y >canvas.height-this.alturaprot){
             this.y = canvas.height -this.alturaprot
         }else{
-            this.vy ++
+            this.vy++
         }
-         if(this.sx<=0){
-            this.sx = 269}
+         if(this.sx>269){
+            this.sx = 0}
 
         ctx.drawImage(this.image,
             this.sx,
             this.sy,
-            (269/7)*(-1) ,
+           595/7,
             240,
             this.x,
             this.y,
@@ -195,149 +284,170 @@ class Personaje2{
     }
 
 
-
-       salta(){
+    salta(){
 
         this.vy = -this.fuerzadesalto*2 
+   
     }
 
     rigth(){
-        this.x -=30;
-        this.move()
-    }
-    left(){
         this.x+=30;
         this.move()
     }
+    left(){
+        this.x-=30;
+        this.move()
+    }
     move(){
-        this.sx+=85
+        this.sx+=595/7
 
     }
 
-    // shoting(){
-
-    //         posx = this.x
-    //         posy -= this.y
-
-
-
+generatebullets() {
+        const positioniBulletx = prot2.x;
+        const positioniBullety =  prot2.y+500
         
-    //     let posx=0, poy=0, velx=0,vely=0, radio=45
-    //     ctx.beginPath()
-    //     ctx.ctx.arc(posx, posy,radio , 0, Math.PI*2, false);
-    //     ctx.fill()
 
+        if (balas2.length >=20 && balas2[10].y<0 ){
+            balas2.splice(0,19)
+        }else{
 
+            balas2.push(new bullet2(
+               (prot2.x-38.5)+(Math.random()*50),
+                (prot2.y+50)+Math.random()*40,
+                Math.random()*35,false))
+        }
 
-
-    // }
-
+    }
+drawbullets() {
+    balas2.forEach(bullet2 => bullet2.draw())
+                    if (varShoot2){
+                    lifeboss=lifeboss -20
+                    console.log(lifeboss);
+                    if(lifeboss<=0){
+                        win()
+                        console.log(lifeboss);
+                    }
+                }
+    }
 }
-
-
-
-
-
 class bullet{
-  constructor(x,y) {
+  constructor(x,y,radio) {
     this.x = x
     this.y = y
     this.vy=-5
-    this.vx =3
-
-
+    this.vx =5
+    this.radio = radio
+     // this.color =`rgb(${Math.floor(Math.random()*255)},
+     //                 ${Math.floor(Math.random()*255)},  
+     //                 ${Math.floor(Math.random()*255)})`
 }
+   
   draw() {
     this.y+= this.vy
     this.x+=this.vx
     if (this.y<0){
-    this.y = this.y
+        this.y = this.y
     }
 
    ctx.beginPath()
-   ctx.arc(this.x, this.y, 35,0,2*Math.PI,false)
-   ctx.fill();
+   ctx.arc(this.x, this.y, this.radio,0,2*Math.PI,false)
+   ctx.fillStyle= `rgb(${Math.floor(Math.random()*255)},
+                     ${Math.floor(Math.random()*255)},  
+                     ${Math.floor(Math.random()*255)})`
+        ctx.fill();
 
-}
-
-hintboss(){
-    if (this.y<0){
-            this.y=canvas.height -120;
-    }else{
-        this.y+=vy
     }
 }
+class bullet2{
+  constructor(x,y,radio) {
+    this.x = x
+    this.y = y
+    this.vy=-5
+    this.vx =5
+    this.radio = radio
+     // this.color =`rgb(${Math.floor(Math.random()*255)},
+     //                 ${Math.floor(Math.random()*255)},  
+     //                 ${Math.floor(Math.random()*255)})`
+    }
+   
+  draw() {
+    this.y+= this.vy
+    this.x-=this.vx
+    if (this.y<0){
+        this.y = this.y
+    }
+
+   ctx.beginPath()
+   ctx.arc(this.x, this.y, this.radio,0,2*Math.PI,false)
+   ctx.fillStyle= `rgb(${Math.floor(Math.random()*255)},
+                     ${Math.floor(Math.random()*255)},  
+                     ${Math.floor(Math.random()*255)})`
+        ctx.fill();
+
+    }
 }
 
 
 const friction = 0.8
 const gravity = 0.98
 const background = new Bg()
-// const boss= new Boss()
+const boss = new FinalBoss()
 const protaGonista = new Personaje(0,canvas.height-240,img)
-const prot2  = new Personaje2(canvas.width-150,canvas.height, img)
-const bolita =  new bullet(protaGonista.x,protaGonista.y)
-const boss = new Boss()
+const prot2  = new Personaje2(canvas.width-85,canvas.height, img)
 
 function update(){
+    frames+=1
     ctx.clearRect(0,0, canvas.width, canvas.height)
         background.draw()
-        boss.draw()
         protaGonista.draw()
         prot2.draw()
-        lifeBars()
 
-        generatebullets()
 
     protaGonista.y +=protaGonista.vy
     protaGonista.x += protaGonista.vx
     protaGonista.y += gravity
-
-
-}
-
-  
-function lifeBars(){
-    ctx.beginPath();
-    ctx.fillStyle= "#FF0000";
-    ctx.fillRect(20, 20, 350, 100)
-    ctx.stroke();
-
-
-
-
-   
-     ctx.fillStyle= "#FF0000";
-     ctx.fillRect(canvas.width-20-350, 20, 350, 100)
-         ctx.stroke();
-
-}
-
-
-   setInterval(update,1000/60)
-
-
-
-
-
-    function generatebullets() {
+    prot2.y +=prot2.vy
+    prot2.x += prot2.vx
+    prot2.y += gravity
+//    bolita.draw()
         
-        const positioniBulletx = protaGonista.x;
-        const positioniBullety =  protaGonista.y+500
-   
-    balas1.push(new bullet(120,canvas.height-240 , false))
+         protaGonista.drawbullets()
+    if (varShoot){
+           protaGonista.generatebullets()
+        }
+        prot2.drawbullets()
+    if (varShoot2){
+           prot2.generatebullets()
+        }
+
+      
+
+        for (let i = 0; i < 3; i++) {
+            new FinalBoss(i).draw()
+        };
+
+        boss.atack()
+
+        if (life1 == 0 || life2== 0 ){
+            gameOver()
+        }
 
     
-  
+
+        
+
+    ctx.beginPath()
+        ctx.rect(20, 30  , 450*life1/50, 50)
+        ctx.rect(canvas.width-475,30,450*life2/50, 50)
+        ctx.lineWidth = '6'
+        ctx.fillStyle = "#FF0000"
+        ctx.stroke()
+        ctx.fill()
+    ctx.closePath()
 }
 
-
-function drawbullets() {
-
-  balas1.forEach(bullet => bullet.draw())
-}
-
-
+    let startGame = setInterval(update,1000/60)
 
 
 
@@ -352,24 +462,107 @@ keys[keyCode]=true
     }else if(keyCode==32){
         protaGonista.salta()
     }else if (keyCode==82) {
-        protaGonista.shoootboos()
-// varShoot=true        
+        varShoot=true     
     }else if(keyCode==80){
         prot2.rigth()
     }else if(keyCode==79){
-        prot2 .left()
-    }else if (keyCode==16) {
-        prot2.shoootboos()
-// varShoot=true        
+        prot2.left()
+    }else if(keyCode==16){
+        prot2.salta()
+    }else if (keyCode==76) {
+         varShoot2=true        
     }
 })
 
 
 document.addEventListener('keyup', ({keyCode})=>{
   varShoot=false
+  varShoot2=false
   keys[keyCode]=false
+  if (balas1.length>31||balas1[0].y==0){
+        balas1.splice(1,balas1.length)
+  }
+if (balas2.length>31||balas2[0].y==0){
+        balas2.splice(1,balas2.length)
+  }
+
    
     
 })
+    
 
 
+
+
+
+
+
+
+
+    
+
+    renderer.setSize(section,y);
+    
+container_Three.appendChild(renderer.domElement);
+
+
+var geometry = new THREE.BoxGeometry( 2, 2, 2);
+var material = new THREE.MeshNormalMaterial();
+var cube     = new THREE.Mesh( geometry, material );
+
+    
+
+
+
+function gameOver(){
+    clearInterval(startGame)
+        ctx.font = "80px Arial"
+        ctx.fillText('Perdiste', canvas.width/2, canvas.height/2)
+
+}
+function win(){
+    clearInterval(startGame)
+            ctx.font = "80px Arial"
+        ctx.fillText('Ganaste', canvas.width/2,  canvas.height/2)
+        scene.add( cube );
+        scene.add( light );
+}
+
+
+
+
+
+
+
+
+camara.position.z = 15;
+
+
+
+
+
+
+
+
+
+
+
+// console.log(THREE.EventDispatcher,
+//  THREE.MOUSE,
+//  THREE.Quaternion,
+//  THREE.Spherical,
+//  THREE.TOUCH,
+//  THREE.Vector2,
+//  THREE.Vector3);
+
+var animate = function () {
+                requestAnimationFrame( animate );
+
+                cube.rotation.x += 0.01;
+                cube.rotation.y += 0.01;
+
+                renderer.render( scene, camara );
+            };
+
+
+            animate();
